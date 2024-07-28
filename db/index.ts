42961812e7fd440be102b452764aab789078ea20
rakibrahman <md.rr.talukder@gmail.com>
2024-07-28 22:07:41 +0600

    added admins route

--- db/index.ts
+++ db/index.ts
@@ -31,7 +31,8 @@ export const initializeDatabase = async () => {
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    image_url TEXT
+    image_url TEXT,
+    role TEXT
 );
 
 
